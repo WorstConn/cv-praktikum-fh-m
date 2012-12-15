18576644a989c3d62c544b66bd7c59be569c158b
julian.cordes <julian.cordes@198433a1-fe4e-e80a-db7f-e614ded9eccd>
2012-12-15 18:31:35 +0000

    Erste Versuche die Hand-Konturen mittels Sobel-Operator und Canny-Edge-Detector zu erhalten!

--- OpenCvPraktikum/main.cpp
+++ OpenCvPraktikum/main.cpp
@@ -44,26 +44,76 @@ int main() {
 }
 #endif
 
+Mat origImage;
+int cannyLow = 1, cannyHigh = 350;
+
 #if 1
+
+void doCanny() {
+    Mat cannyImage, cannyImageInv;
+    
+    Canny(origImage, cannyImage, cannyLow, cannyHigh);
+    threshold(cannyImage, cannyImageInv, 128, 255, THRESH_BINARY_INV);
+    imshow("Canny", cannyImageInv);
+}
+
+void callbackCanny(int pos) {
+    doCanny();
+    DBG("canny erneut mit lowThreshold = %d, highThreshold = %d angewendet!", cannyLow, cannyHigh);
+}
+
+
+
 int main() {
     CvHelper *help = CvHelper::getInstance();
     
-    Mat image = imread("lena.jpg");
+    origImage = imread("bild.jpg");
+    Mat sobelX, sobelY, sobel;
+   
     
-    MatND histo =help->makeHist(&image);
-    
-    Mat histImg = help->makeHistImage(histo);
-    
-   /* for (int i = 0; i < 256; i++) {
-        cout << "Value " << i << " = " << histo.at<float>(i) << endl;
-    }*/
-    
+    MatND histo;
+    Mat histImg;
     namedWindow("Bild");
-    imshow("Bild", image);
-    
-    
+    namedWindow("BinBild");
     namedWindow("Histogram");
+    namedWindow("SobelX");
+    namedWindow("SobelY");
+    namedWindow("Canny");
+   
+    cvCreateTrackbar( "CannyTrack", "Canny",
+                      &cannyLow, 255, callbackCanny );
+    cvCreateTrackbar( "CannyTrackHigh", "Canny",
+                      &cannyHigh, 400, callbackCanny);
+    
+
+    
+    Sobel(origImage, sobelX, CV_8U,1,0,3,0.4,128);
+    Sobel(origImage, sobelY, CV_8U,0,1,3,0.4,128);
+    
+    sobel = sobelX;
+    double sobelMin, sobelMax;
+    
+    minMaxLoc(sobel, &sobelMin, &sobelMax);
+    Mat image, binImage;
+    sobel.convertTo(image, CV_8U, -255.0/sobelMax, 255);
+    threshold(image,binImage,30,255, cv::THRESH_BINARY);
+    
+    Mat cannyImage, cannyImageInv;
+    
+    Canny(origImage, cannyImage, 125, 350);
+    threshold(cannyImage, cannyImageInv, 128, 255, THRESH_BINARY_INV);
+    
+    
+    
+    histo =help->makeHist(&image);
+    histImg = help->makeHistImage(histo);
+    imshow("Bild", image);
+    imshow("BinBild", binImage);
+    imshow("SobelX", sobelX);
+    imshow("SobelY", sobelY);
     imshow("Histogram", histImg);
+    imshow("Canny", cannyImageInv);
+    waitKey(300);
     
     waitKey(0);
     
